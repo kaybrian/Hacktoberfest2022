d8f9056a36d8d8a06a1ecb7d5e8391388ab69cb7
Santigie Sankoh <santigiesankoh233@gmail.com>
2022-10-23 00:58:51 +0000

    Create square.py
    Algorithm

---
+++ square.py
@@ -0,0 +1,12 @@
+# square root by hand solving 
+
+count = 0
+
+def main():
+    count = int(input()) **2
+    while count > 0:
+        print(f" the square root of {input()} is {count}")
+        main()
+#4 = 8, 16, 32..
+
+main()
